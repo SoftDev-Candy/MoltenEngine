1053b88e91343460b8f78aea359f9a12c843a3c9
SoftDev-Candy <swastiktoprani@gmail.com>
2025-11-10 16:38:48 +0100

    Added GLAD and it's required Cmake file

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,37 @@
 #include<iostream>
 #include<GLFW/glfw3.h>
-int main()
-{
 
+int main(void) {
+    GLFWwindow* window;
 
+//Initialize the Library
+    if (!glfwInit()) {
+        std::cout << "Failed to initialize GLFW" << std::endl;
+        return -1;
+    }
+
+    //create a windowed mode window and with openGL //
+    window = glfwCreateWindow(640, 480, "Hello World", nullptr, nullptr);
+    if (!window)
+    {
+        glfwTerminate();
+        return -1;
+    }
+
+    glfwMakeContextCurrent(window);
+    std::cout << window << std::endl;
+
+    while (!glfwWindowShouldClose(window))
+    {
+
+        glClear(GL_COLOR_BUFFER_BIT);
+
+        glfwSwapBuffers(window);
+
+        glfwPollEvents();
+
+    }
+
+    glfwTerminate();
+    return 0;
 }
